86c832b49b17942f4e6703a05afc204684eade74
Bruno Silva <brunogs.sp@gmail.com>
2017-05-07 16:00:50 -0300

    init chapter02, implemented check_token and update_token

---
+++ redis-in-action/chapter02/login_cookie_caching.py
@@ -0,0 +1,13 @@
+
+
+
+def check_token(conn, token):
+    return conn.hget('login:', token)
+
+def update_token(conn, token, user, item=None):
+    timestamp = time.time()
+    conn.hset('login:', token, user)
+    conn.zadd('recent:', token, timestamp)
+    if item:
+        conn.zadd('viewed:' + token, item, timestamp)
+        conn.zremrangebyrank('viewed:' + token, 0, -26)
