3556924dd09ebd0ea6beaf285188256b5d9fe1eb
Bruno Silva <brunogs.sp@gmail.com>
2017-05-06 21:03:53 -0300

    add test from repo: https://github.com/josiahcarlson/redis-in-action/tree/master/python

--- redis-in-action/chapter01/voting_article.py
+++ redis-in-action/chapter01/voting_article.py
@@ -1,3 +1,5 @@
+import time
+import unittest
 
 ONE_WEEK_IN_SECONDS = 7 * 86400
 VOTE_SCORE = 432
@@ -66,3 +68,67 @@ def get_group_articles(conn, group, page, order='score:'):
         )
         conn.expire(key, 60)
     return get_articles(conn, page, key)
+
+
+
+
+#-------------------------Simple Tests---------------------------------------------
+
+
+
+
+class TestCh01(unittest.TestCase):
+    def setUp(self):
+        import redis
+        self.conn = redis.Redis(db=15)
+
+    def tearDown(self):
+        del self.conn
+        print
+        print
+
+    def test_article_functionality(self):
+        conn = self.conn
+        import pprint
+
+        article_id = str(post_article(conn, 'username', 'A title', 'http://www.google.com'))
+        print "We posted a new article with id:", article_id
+        print
+        self.assertTrue(article_id)
+
+        print "Its HASH looks like:"
+        r = conn.hgetall('article:' + article_id)
+        print r
+        print
+        self.assertTrue(r)
+
+        article_vote(conn, 'other_user', 'article:' + article_id)
+        print "We voted for the article, it now has votes:",
+        v = int(conn.hget('article:' + article_id, 'votes'))
+        print v
+        print
+        self.assertTrue(v > 1)
+
+        print "The currently highest-scoring articles are:"
+        articles = get_articles(conn, 1)
+        pprint.pprint(articles)
+        print
+
+        self.assertTrue(len(articles) >= 1)
+
+        add_remove_groups(conn, article_id, ['new-group'])
+        print "We added the article to a new group, other articles include:"
+        articles = get_group_articles(conn, 'new-group', 1)
+        pprint.pprint(articles)
+        print
+        self.assertTrue(len(articles) >= 1)
+
+        to_del = (
+            conn.keys('time:*') + conn.keys('voted:*') + conn.keys('score:*') + 
+            conn.keys('article:*') + conn.keys('group:*')
+        )
+        if to_del:
+            conn.delete(*to_del)
+
+if __name__ == '__main__':
+    unittest.main()
