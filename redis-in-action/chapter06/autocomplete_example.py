08bf17bf98692f0af7a04712abeed7b02a3a7cd9
Bruno Silva <bruno.silva@belezanaweb.com.br>
2017-08-15 09:59:54 -0300

    add: fetch autocomplete

--- redis-in-action/chapter06/autocomplete_example.py
+++ redis-in-action/chapter06/autocomplete_example.py
@@ -12,13 +12,21 @@ def add_update_contact(conn, user, contact):
 def remove_contact(conn, user, contact):
     conn.lrem('recent:' + user, contact)
 
+def fetch_autocomplete_list(conn, user, prefix):
+    candidates = conn.lrange('recent:' + user, 0, -1)
+    matches = []
+    for candidate in candidates:
+        if candidate.lower().startswith(prefix):
+            matches.append(candidate)
+    return matches
+
 
 print "Testing"
 conn = redis.Redis()
 add_update_contact(conn, 'brunogs', 'Virginia')
 add_update_contact(conn, 'brunogs', 'Nalva')
 add_update_contact(conn, 'brunogs', 'Zeca')
-print conn.lrange('recent:brunogs', 0, -1)
+print fetch_autocomplete_list(conn, 'brunogs', 'vir')
 remove_contact(conn, 'brunogs', 'Virginia')
 print conn.lrange('recent:brunogs', 0, -1)
 
