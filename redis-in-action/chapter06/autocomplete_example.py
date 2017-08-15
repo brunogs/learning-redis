f69140394f6204eeb6d0895200c2a330d1da12a6
Bruno Silva <bruno.silva@belezanaweb.com.br>
2017-08-15 10:30:14 -0300

    add: autocomplete with zset

--- redis-in-action/chapter06/autocomplete_example.py
+++ redis-in-action/chapter06/autocomplete_example.py
@@ -1,5 +1,6 @@
 import redis
 import bisect
+import uuid
 
 
 def add_update_contact(conn, user, contact):
@@ -28,6 +29,31 @@ def find_prefix_range(prefix):
     posn = bisect.bisect_left(valid_characters, prefix[-1:])
     suffix = valid_characters[(posn or 1) - 1]
     return prefix[:-1] + suffix + '{', prefix + '{'
+
+
+def autocomplete_on_prefix(conn, guild, prefix):
+    start, end = find_prefix_range(prefix)
+    identifier = str(uuid.uuid4())
+    start += identifier
+    end += identifier
+    zset_name = 'members:' + guild
+
+    conn.zadd(zset_name, start, 0, end, 0)
+    pipeline = conn.pipeline(True)
+    while 1 :
+        try:
+             pipeline.watch(zset_name)
+             sindex = pipeline.zrank(zset_name, start)
+             eindex = pipeline.zrank(zset_name, end)
+             erange = min(sindex + 9, eindex - 2)
+             pipeline.multi()
+             pipeline.zrem(zset_name, start, end)
+             pipeline.zrange(zset_name, sindex, erange)
+             items = pipeline.execute()[-1]
+             break
+        except redis.exceptions.WatchError:
+            continue
+    return [item for item in items if '{' not in item]
 
 
 print "Testing"
